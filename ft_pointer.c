e14f3f5a1e4c70bfa78d6b6dc4f2004505d68abc
Guillaume Lacroix <glacroix@c3r9s4.42madrid.com>
2022-12-15 17:17:49 +0100

    refactor + norminette

--- ft_pointer.c
+++ ft_pointer.c
@@ -6,7 +6,7 @@
 /*   By: glacroix <glacroix@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/10 16:35:24 by glacroix          #+#    #+#             */
-/*   Updated: 2022/12/14 16:18:59 by glacroix         ###   ########.fr       */
+/*   Updated: 2022/12/15 17:15:47 by glacroix         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,17 +28,17 @@ int	ft_pointer(size_t n, char *base)
 
 	i = 0;
 	result = 0;
-    if (n == 0)
+	if (n == 0)
 		result += write(1, "0x0", 3);
 	if (n != 0)
 		result += write(1, "0x", 2);
-    while (n)
-    {
-        nbr_final[i] = n % 16;
-        n = n / 16;
-        i++;
-    }
-    while (--i >= 0)
-        result += ft_putchar(base[nbr_final[i]]);
-    return (result);
+	while (n)
+	{
+		nbr_final[i] = n % 16;
+		n = n / 16;
+		i++;
+	}
+	while (--i >= 0)
+		result += ft_putchar(base[nbr_final[i]]);
+	return (result);
 }
--- ft_putnbr_hex.c
+++ ft_putnbr_hex.c
@@ -6,15 +6,15 @@
 /*   By: glacroix <glacroix@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/08 16:35:24 by glacroix          #+#    #+#             */
-/*   Updated: 2022/12/14 16:21:41 by glacroix         ###   ########.fr       */
+/*   Updated: 2022/12/15 17:16:50 by glacroix         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
 /**
- * It takes an unsigned int and a string of characters, and prints the unsigned int
- * in hexadecimal form using the characters in the string as the base
+ * It takes an unsigned int and a string of characters, and prints the unsigned 
+ * int in hexadecimal form using the characters in the string as the base
  * 
  * @param n the number to be converted
  * @param base the base to convert to
@@ -23,21 +23,21 @@
  */
 int	ft_putnbr_hex(unsigned int n, char *base)
 {
-    int	nbr_final[16];
-    int	i;
-    int	result;
-	
-    i = 0;
-    result = 0;
-    if (n == 0)
-        result += ft_putchar('0');
-    while (n)
-    {
-        nbr_final[i] = n % 16;
-        n = n / 16;
-        i++;
-    }
-    while (--i >= 0)
-        result += ft_putchar(base[nbr_final[i]]);
-    return (result);
+	int	nbr_final[16];
+	int	i;
+	int	result;
+
+	i = 0;
+	result = 0;
+	if (n == 0)
+		result += ft_putchar('0');
+	while (n)
+	{
+		nbr_final[i] = n % 16;
+		n = n / 16;
+		i++;
+	}
+	while (--i >= 0)
+		result += ft_putchar(base[nbr_final[i]]);
+	return (result);
 }
